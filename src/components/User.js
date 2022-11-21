bea6cc6a3d1d97f6c26d20ececbf2d8d8d5ee7b5
Anisha-Karmacharya <anisha.karmacharya.01@gmail.com>
2022-11-21 11:04:18 +0545

    added row and col tag to manage the display of cards in the page

--- src/components/User.js
+++ src/components/User.js
@@ -1,29 +1,31 @@
 import React from "react";
-import { Card } from "antd";
+import { Row, Col, Card } from "antd";
 const User = ({ userData }) => {
   const { Meta } = Card;
   return (
     <div>
-      {userData &&
-        userData.length > 0 &&
-        userData.map((user, index) => (
-          <div key={index}>
-            <Card>
-              <div key={index}>
-                <Meta
-                  title={user.name}
-                  description={[
-                    <div key={index}>
-                      <p> {user.email}</p>
-                      <p>{user.phone}</p>
-                      <p> {user.website}</p>
-                    </div>
-                  ]}
-                />
-              </div>
-            </Card>
-          </div>
-        ))}
+      <Row>
+        {userData &&
+          userData.length > 0 &&
+          userData.map((user, index) => (
+            <div key={index}>
+              <Col>
+                <Card>
+                    <Meta
+                      title={user.name}
+                      description={[
+                        <div key={index}>
+                          <p> {user.email}</p>
+                          <p>{user.phone}</p>
+                          <p> {user.website}</p>
+                        </div>,
+                      ]}
+                    />
+                </Card>
+              </Col>
+            </div>
+          ))}
+      </Row>
     </div>
   );
 };
