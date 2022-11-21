90c0b12ef7247154ea3f499c8de6120bfe008379
Anisha-Karmacharya <anisha.karmacharya.01@gmail.com>
2022-11-21 11:12:22 +0545

    added span to manage the width according to screensize and removed div before the Col tag

--- src/components/User.js
+++ src/components/User.js
@@ -4,33 +4,41 @@ const User = ({ userData }) => {
   const { Meta } = Card;
   return (
     <div>
-      <Row gutter={[16, 16]}>
+      <Row
+        gutter={[
+          { xs: 8, sm: 16, md: 24, lg: 32 },
+          { xs: 8, sm: 16, md: 24, lg: 32 },
+        ]}
+      >
         {userData &&
           userData.length > 0 &&
           userData.map((user, index) => (
-            <div key={index}>
-              <Col>
-                <Card
-                    cover={
-                        <img
-                        alt="user-image"
-                        src="https://avatars.dicebear.com/api/micah/your-custom-seed.svg"
-                        />
-                    }
-                >
-                    <Meta
-                      title={user.name}
-                      description={[
-                        <div key={index}>
-                          <p> {user.email}</p>
-                          <p>{user.phone}</p>
-                          <p> {user.website}</p>
-                        </div>,
-                      ]}
-                    />
-                </Card>
-              </Col>
-            </div>
+            <Col
+              xs={{ span: 24 }}
+              md={{ span: 8 }}
+              lg={{ span: 6 }}
+              key={index}
+            >
+              <Card
+                cover={
+                  <img
+                    alt="user-image"
+                    src="https://avatars.dicebear.com/api/micah/your-custom-seed.svg"
+                  />
+                }
+              >
+                <Meta
+                  title={user.name}
+                  description={[
+                    <div key={index}>
+                      <p> {user.email}</p>
+                      <p>{user.phone}</p>
+                      <p> {user.website}</p>
+                    </div>,
+                  ]}
+                />
+              </Card>
+            </Col>
           ))}
       </Row>
     </div>
